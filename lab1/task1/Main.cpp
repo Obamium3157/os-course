38ea7bea4b9bff8b4fab59de31329f42e417e95c
obamium3157 <p_zhdanov@icloud.com>
2026-09-05 06:02:17 +0300

    L1T1 Переписал код в виде класса

--- lab1/task1/Main.cpp
+++ lab1/task1/Main.cpp
@@ -1,22 +1,7 @@
-#include <iostream>
-
-#include "SysInfoWin.h"
-
-void PrintInfo()
-{
-  PrintSystemVersion();
-  PrintComputerName();
-  PrintUserName();
-  PrintArchitecture();
-  PrintRAM(Precision::GB);
-  PrintVirtualMemory(Precision::GB);
-  PrintMemoryLoad();
-  PrintPageFile(Precision::GB);
-  PrintProcessorsAmount();
-  PrintDrives(Precision::GB);
-}
+#include "SysInfo.h"
 
 int main()
 {
-  PrintInfo();
+  const SysInfo sysInfo(Measurement::MB);
+  sysInfo.PrintInfo();
 }
--- lab1/task1/SysInfoWin.h
+++ lab1/task1/SysInfoWin.h
@@ -1,22 +1,15 @@
-#ifndef OS_COURSE_SYSINFOWIN_H
-#define OS_COURSE_SYSINFOWIN_H
-
-enum class Precision
-{
-  KB = 1024ULL,
-  MB = 1024ULL * KB,
-  GB = 1024ULL * MB,
-};
-
-void PrintSystemVersion();
-void PrintComputerName();
-void PrintUserName();
-void PrintArchitecture();
-void PrintRAM(Precision precision);
-void PrintVirtualMemory(Precision precision);
-void PrintMemoryLoad();
-void PrintPageFile(Precision precision);
-void PrintProcessorsAmount();
-void PrintDrives(Precision precision);
-
-#endif //OS_COURSE_SYSINFOWIN_H
+// #ifndef OS_COURSE_SYSINFOWIN_H
+// #define OS_COURSE_SYSINFOWIN_H
+//
+// void PrintSystemVersion();
+// void PrintComputerName();
+// void PrintUserName();
+// void PrintArchitecture();
+// void PrintRAM(Measurement measurement);
+// void PrintVirtualMemory(Measurement measurement);
+// void PrintMemoryLoad();
+// void PrintPageFile(Measurement measurement);
+// void PrintProcessorsAmount();
+// void PrintDrives(Measurement measurement);
+//
+// #endif //OS_COURSE_SYSINFOWIN_H
