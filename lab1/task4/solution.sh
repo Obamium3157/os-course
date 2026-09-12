828da67f5eecc876d774f125de74385754c15e52
obamium3157 <p_zhdanov@icloud.com>
2026-09-12 09:22:49 +0300

    L1T4 Добавил комментарии к четвертому заданию

--- lab1/task4/solution.sh
+++ lab1/task4/solution.sh
@@ -2,6 +2,7 @@
 
 tar -czf proj.tar.gz proj
 if [ -d out ]; then
+  # TODO: уточнить применение флагов
   read -r -p "Directory out already exists. Recreate? (y/N) " ans
   if [[ $ans =~ ^[Yy]$ ]]; then
     rm -rf out
@@ -12,10 +13,10 @@ fi
 mkdir out
 cp proj.tar.gz out/
 cd out
-tar -xzf proj.tar.gz --strip-components=1
+tar -xzf proj.tar.gz --strip-components=1 # TODO: что будет, если написать 2. Показать примеры
 rm -rf proj.tar.gz
 mkdir include src build
 mv lib.h include/
 mv lib.cpp main.cpp src/
-g++ src/main.cpp src/lib.cpp -Iinclude -o build/proj
+g++ src/main.cpp src/lib.cpp -Iinclude -o build/proj # TODO: доуточнить про Iinclude
 echo "30 12" | ./build/proj > stdout.txt
