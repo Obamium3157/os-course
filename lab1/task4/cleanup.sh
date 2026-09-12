c2f0ab3b9e0e943b4663cf49571eefc0196cf651
obamium3157 <p_zhdanov@icloud.com>
2026-09-12 06:57:56 +0300

    L1T4 Решил задание

---
+++ lab1/task4/cleanup.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+
+rm -rf out
+rm -rf proj.tar.gz
---
+++ lab1/task4/solution.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+tar -czf proj.tar.gz proj
+if [ -d out ]; then
+  read -r -p "Directory out already exists. Recreate? (y/N) " ans
+  if [[ $ans =~ ^[Yy]$ ]]; then
+    rm -rf out
+  else
+    exit 1
+  fi
+fi
+mkdir out
+cp proj.tar.gz out/
+cd out
+tar -xzf proj.tar.gz --strip-components=1
+rm -rf proj.tar.gz
+mkdir include src build
+mv lib.h include/
+mv lib.cpp main.cpp src/
+g++ src/main.cpp src/lib.cpp -Iinclude -o build/proj
+echo "30 12" | ./build/proj > stdout.txt
